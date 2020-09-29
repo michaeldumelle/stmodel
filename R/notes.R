cef6d7576080657bda855adc553f50d33b9db291
Michael Dumelle <mjdumelle@gmail.com>
2020-09-28 21:00:46 -0700

    added notes

---
+++ R/notes.R
@@ -0,0 +1,22 @@
+stlm_construct <- function(estmethod) {
+  new_estmethod <- structure(list(), class = estmethod)
+}
+
+stlm <- function(t) {
+  UseMethod("stlm", object = t)
+}
+
+stlm.loglik <- function() {
+  x <- TRUE
+  return(x)
+}
+
+stlm("loglik")
+
+stlm.gam <- function()
+stlm.svls <- function()
+
+
+#i think i should just use switch
+
+# stlm_sv, stlm_loglik, stlm_gam
