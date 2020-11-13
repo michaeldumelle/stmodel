a8bd3a6e1edc13f95bc355e06bb9789cd4599548
Michael Dumelle <mjdumelle@gmail.com>
2020-11-13 10:57:57 -0800

    commented and tidied functions

--- R/varest.R
+++ R/varest.R
@@ -1,13 +1,32 @@
-varest <- function(invert_object, invert_output){
+varest <- function(invert_object, invert_output) {
+
+  # calling the appropriate generic
   UseMethod("varest", object = invert_object)
 }
 
-varest.reml <- function(invert_object, invert_output){
+# compute the overall variance for reml estimation
+varest.reml <- function(invert_object, invert_output) {
+
+  # store the sample size
   n <- nrow(invert_output$sigmainv_o)
-  betaest_output <- betaest(xo = invert_object$xo, sigmainv_xyo = invert_output$sigmainv_o,
-                            condition = invert_object$condition, return_estlist = TRUE)
-  r_siginv_r <- t(invert_object$yo - invert_object$xo %*% betaest_output$betahat) %*%
-    (betaest_output$estlist$sigmainv_yo - betaest_output$estlist$sigmainv_xo %*% betaest_output$betahat)
+
+  # compute the beta estimate and return estmation information (via return_estlist)
+  betaest_output <- betaest(
+    xo = invert_object$xo,
+    sigmainv_xyo = invert_output$sigmainv_o,
+    condition = invert_object$condition,
+    return_estlist = TRUE
+  )
+
+  # compute sigma inverse times the residual vector
+  siginv_r <- betaest_output$estlist$sigmainv_yo - betaest_output$estlist$sigmainv_xo %*% betaest_output$betahat
+
+  # compute the quadratic residual function
+  r_siginv_r <- t(invert_object$yo - invert_object$xo %*% betaest_output$betahat) %*% siginv_r
+
+  # compute the overall variance
   ws_l2 <- as.vector(r_siginv_r / (n - betaest_output$estlist$p))
-  return(ws_l2) # thanks russ and john!
+
+  # return the overall variance
+  return(ws_l2) # thanks Russ and John!
 }
